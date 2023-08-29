452307c7fdc7238661c1f70924616a9d5508de9f
lovely suma <sumalatha11@gmail.com>
2023-08-29 18:38:19 +0530

    first commit

---
+++ Assesment_12/Assesment_12/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assesment_12.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string PublicationDate { get; set; }
+
+        // Foreign key to relate comments to a post
+        public int PostId { get; set; }
+        
+
+    }
+}
